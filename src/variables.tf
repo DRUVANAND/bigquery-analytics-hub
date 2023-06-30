1baf2449da2bbc58b4bd4056af9b7650be661449
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-06-30 17:03:53 +0530

    Create variables.tf

---
+++ src/variables.tf
@@ -0,0 +1,25 @@
+variable "project" {
+  description = "The region where the resources will be created"
+  default     = "pro-signal-386907"
+}
+
+variable "region" {
+  description = "The region where the resources will be created"
+  default     = "us-central1"
+}
+
+variable "data_exchange_id" {
+  description = "ID of the data_exchange"
+  default     = "my_data_exchange"
+}
+
+variable "listing_id" {
+  description = "ID of the listing_id"
+  default     = "my_listing"
+}
+
+variable "dataset_id" {
+  description = "ID of the dataset_id"
+  default     = "purchasing"
+}
+
